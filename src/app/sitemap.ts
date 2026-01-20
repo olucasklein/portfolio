c093b809c39bf5a286d02cdeaa1d888a96e6abee
Lucas <olucasklein@hotmail.com>
2026-01-19 21:38:56 -0300

    feat: refactor sitemap function to support multiple base URLs

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,15 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://lucasklein.dev'
-  
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
+  const baseUrls = [
+    'https://lucasklein.dev',
+    'https://www.lucaskleindev.com',
   ]
+  
+  return baseUrls.map(baseUrl => ({
+    url: baseUrl,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 1,
+  }))
 }
